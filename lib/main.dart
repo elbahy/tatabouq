dcc9bc43d390a15d8d55ce9ff1487694b2586440
elbahy <vb.supporter@gmail.com>
2022-12-21 23:09:59 +0200

    change action to change two images in the same time by one click

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-        backgroundColor: Colors.blue,
-        
-        body: MyImages()),
+    home: Scaffold(backgroundColor: Colors.blue, body: MyImages()),
   ));
 }
 
@@ -21,17 +18,31 @@ class _MyImagesState extends State<MyImages> {
   int rightImagesNumber = 5;
   String res = "إضغط على احدى الصورتين";
   int point = 0;
+
+  void changeImages() {
+    lefImagesNumber = Random().nextInt(9) + 1;
+    rightImagesNumber = Random().nextInt(9) + 1;
+    res = "حاول مرة اخرى";
+    if (lefImagesNumber == rightImagesNumber) {
+      res = "مبرووووك لقد ربحت";
+      point++;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Text("$point",style: TextStyle(
+        Text(
+          "$point",
+          style: TextStyle(
               fontSize: 30,
               color: Colors.white,
               fontWeight: FontWeight.bold,
-              fontFamily: "almarai"),),
+              fontFamily: "almarai"),
+        ),
         Text(
           "$res",
           style: TextStyle(
@@ -47,12 +58,7 @@ class _MyImagesState extends State<MyImages> {
               child: Image.asset("images/image-$lefImagesNumber.png"),
               onPressed: () {
                 setState(() {
-                  lefImagesNumber = Random().nextInt(9) + 1;
-                  res = "حاول مرة اخرى";
-                  if (lefImagesNumber == rightImagesNumber) {
-                    res = "مبرووووك لقد ربحت";
-                    point++;
-                  }
+                  changeImages();
                 });
               },
             )),
@@ -61,12 +67,7 @@ class _MyImagesState extends State<MyImages> {
               child: Image.asset("images/image-$rightImagesNumber.png"),
               onPressed: () {
                 setState(() {
-                  rightImagesNumber = Random().nextInt(9) + 1;
-                  res = "حاول مرة اخرى";
-                  if (lefImagesNumber == rightImagesNumber) {
-                    res = "مبرووووك لقد ربحت";
-                    point++;
-                  }
+                  changeImages();
                 });
               },
             )),
